5ac8bbf976adb3e4ee1a4e8a57f49236e40375f4
Maicss <maicss@126.com>
2022-05-19 19:20:02 +0800

    新增 基本的帖文展示功能

--- src/main/java/com/lemming/lemming/dao/PostDao.java
+++ src/main/java/com/lemming/lemming/dao/PostDao.java
@@ -1,9 +1,11 @@
 package com.lemming.lemming.dao;
 
 import com.lemming.lemming.DataBaseConnect;
+import com.lemming.lemming.bean.Post;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PostDao {
@@ -15,7 +17,7 @@ public class PostDao {
             return false;
         }
 
-        String sql = "insert into post_info(title, post, user_id, reg_time) VALUES (?,?,?,NOW())";
+        String sql = "insert into post_info(title, post, user_id, post_time) VALUES (?,?,?,NOW())";
         try {
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1,title);
@@ -25,6 +27,32 @@ public class PostDao {
             connection.close();
             preparedStatement.close();
             return res > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Post getPostById(int id){
+        Post post = new Post();
+        Connection connection = DataBaseConnect.getConnection();
+
+        PreparedStatement preparedStatement = null;
+        if (connection==null){
+            return null;
+        }
+
+        String sql = "select * from post_info where id=?";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,id);
+            ResultSet res = preparedStatement.executeQuery();
+            if (res.next()){
+                post.parseFromResultSet(res);
+            }else {
+                post = null;
+            }
+            connection.close();
+            preparedStatement.close();
+            return post;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
