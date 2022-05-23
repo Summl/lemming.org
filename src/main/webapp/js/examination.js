9e1634e4bc614e629cedec0290d8f1a54e7acfd9
云止 <2838368526@qq.com>
2022-05-23 16:21:23 +0800

    优化了试卷界面

--- src/main/webapp/js/examination.js
+++ src/main/webapp/js/examination.js
@@ -3,23 +3,36 @@ var obj = JSON.parse(json);
 console.log(obj.course);
 console.log(obj.fruits);*/
 
-window.onload=function(){
-    var url = "js/my.json";     //json文件的url
-    var request = new XMLHttpRequest();
+window.onload = function(){
+    let url = "js/my.json";     //json文件的url
+    let request = new XMLHttpRequest();
     request.open("get",url);    //设置请求方法与路径
     request.send(null);     //不发送数据到服务器
     request.onload = function (){
         if(request.status===200){
-            var json = JSON.parse(request.responseText);
-            for(var i=0;i<json.length;i++){
-                document.write(json[i].id);
-                document.writeln(json[i].title);
-                document.write('<br>');
-                for(var j=0;j<json[i].option.length;j++) {
-                    document.writeln(json[i].option[j]);
-                    document.write('<br>');
+            let json = JSON.parse(request.responseText);
+            for(let i=0;i<json.length;i++){
+                let out = document.getElementById("out");
+                let title = document.createElement("p");
+                title.innerText = json[i].title;
+                title.classList.add("title");
+                out.append(title);
+                console.log(json[i].id);
+                // document.write(json[i].title);
+                // document.write('<br>');
+                for(let j=0;j<json[i].option.length;j++) {
+                    let option = document.createElement("p");
+                    option.innerText = json[i].option[j];
+                    option.classList.add("option");
+                    out.append(option);
                 }
+                let br = document.createElement("br");
+                br.innerText = "";
+                out.append(br);
             }
+            let bt = document.createElement("button");
+            bt.innerText = "提交";
+            out.append(bt);
         }
     }
 }
